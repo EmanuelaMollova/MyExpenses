b5f8d623bc4ae4aec0f3885cd56b7cf30ad4eda1
EmanuelaMollova <emanuela.mollova@gmail.com>
2014-02-07 23:07:54 +0200

    Add protected! method and unauthorized page

--- helpers/authentication_helpers.rb
+++ helpers/authentication_helpers.rb
@@ -11,5 +11,9 @@ module MyExpenses
     def passwords_dont_match(password, repeated_password)
       "The passwords don't match." if password != repeated_password
     end
+
+    def protected!
+      halt 401, (haml :unauthorized) unless user_logged?
+    end
   end
 end
