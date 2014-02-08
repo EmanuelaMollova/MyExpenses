d0997d16d60a4b24ae8fe5ccec94133eeb7be2ea
EmanuelaMollova <emanuela.mollova@gmail.com>
2014-02-08 12:50:47 +0200

    Move MyExpensesBase from config.ru to my_app.rb

---
+++ my_app.rb
@@ -0,0 +1,29 @@
+require 'rubygems'
+require 'bundler'
+
+module MyExpenses
+  Bundler.require
+
+  class MyExpensesBase < Sinatra::Base
+    set :views, File.expand_path('../views', __FILE__)
+    enable :sessions
+
+    not_found do
+      haml :not_found
+    end
+  end
+end
+
+Dir.glob('./{models,helpers,controllers}/*.rb').each { |file| require file }
+
+DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+
+DataMapper.auto_upgrade!
+DataMapper.finalize
+
+PATHS = {
+  '/'           => MyExpenses::WebsiteController,
+  '/user'       => MyExpenses::AuthenticationController,
+  '/categories' => MyExpenses::CategoryController,
+  '/expenses'   => MyExpenses::ExpenseController
+}
